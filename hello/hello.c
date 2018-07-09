24718724b4a4b64ee30d57c9ddd2de21671fd492
Radovan Prodanovic <chele85@gmail.com>
2018-07-09 15:26:18 +0200

    provera asserta

--- hello/hello.c
+++ hello/hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <assert.h>
+#include "string.h"
 
 int main ()
 {
-	printf("TEST PROBA ewqweqw\n");
+	char *s = "OVO JE TEST";
+	int len = strlen(s);
+	printf("%i\n", len);
+	printf("%s\n", s);
+	assert(len==10);
 	return 0;
 }
