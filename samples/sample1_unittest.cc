bd446cd3ac12e8ec78960f52e916932d115f3a7e
Radovan Prodanovic <chele85@gmail.com>
2018-07-16 13:21:28 +0200

    blabla_4

--- samples/sample1_unittest.cc
+++ samples/sample1_unittest.cc
@@ -79,7 +79,7 @@ namespace {
 TEST(FactorialTest, Negative) {
   // This test is named "Negative", and belongs to the "FactorialTest"
   // test case.
-  EXPECT_EQ(1, Factorial(25));
+  EXPECT_EQ(1, Factorial(25)); 
   EXPECT_EQ(1, Factorial(-1));
   EXPECT_GT(Factorial(-10), 0);
 
